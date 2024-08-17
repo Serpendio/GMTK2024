b9fd195dec6afb12a3d239ab4e9e7b609997f54f
EDMUND LEUNG <el273447@falmouth.ac.uk>
2024-08-17 10:00:34 +0100

    added credits button

--- Assets/_Scripts/MainMenu.cs
+++ Assets/_Scripts/MainMenu.cs
@@ -5,8 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] GameObject mainMenu;
+    [SerializeField] GameObject settings;
+    [SerializeField] GameObject credis;
+    private void Start()
+    {
+        mainMenu.SetActive(true);
+        settings.SetActive(false);
+        credis.SetActive(false);
+    }
     public void StartGame()
     {
         SceneManager.LoadScene(1);
+    }
+    public void Settings()
+    {
+
+    }
+    public void Credis()
+    {
+        mainMenu.SetActive(false);
+        credis.SetActive(true);
+    }
+    public void backToMenu()
+    {
+        settings.SetActive(false);
+        credis.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }

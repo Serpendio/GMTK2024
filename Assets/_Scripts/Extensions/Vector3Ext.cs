b7fe6ab754ff2251fc5cc41fe1ac8821dad79bc0
SSeelos <sebastian.seelos@gmx.de>
2024-08-17 21:52:03 +0200

    vector-/directionTo 3D

--- Assets/_Scripts/Extensions/Vector3Ext.cs
+++ Assets/_Scripts/Extensions/Vector3Ext.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 
 public static class Vector3Ext
 {
-
+    public static Vector3 VectorTo(this Vector3 from, Vector3 to)
+        => to - from;
+    public static Vector3 DirectionTo(this Vector3 from, Vector3 to)
+        => from.VectorTo(to).normalized;
     public static Vector3 With(this Vector3 original, float? x = null, float? y = null, float? z = null)
         => new Vector3(
             (float)(x == null ? original.x : x),
@@ -17,7 +20,7 @@ public static class Vector3Ext
         => Vector3.Distance(from, to);
 
     public static bool IsGrounded(this Vector3 position, float offset)
-        => Physics2D.Raycast(position, -Vector3.up, offset);
+        => Physics2D.Raycast(position, Vector3.down, offset);
     public static bool IsGrounded(this Vector3 position, Collider2D collider, float offset)
         => position.IsGrounded(collider.bounds.extents.y + offset);
 }

0ee773cdda1411a7ae322bad9416b79d8dc346ec
SSeelos <sebastian.seelos@gmx.de>
2024-08-17 20:16:49 +0200

    Transform/position distance ext

--- Assets/_Scripts/Extensions/TransformExt.cs
+++ Assets/_Scripts/Extensions/TransformExt.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public static class TransformExt
 {
+    public static float Distance(this Transform from, Transform to)
+        => from.position.Distance(to.position);
     public static bool IsGrounded(this Transform transform, Collider2D collider, float offset)
         => transform.position.IsGrounded(collider, offset);
 }
--- Assets/_Scripts/Extensions/Vector3Ext.cs
+++ Assets/_Scripts/Extensions/Vector3Ext.cs
@@ -1,9 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public static class Vector3Ext
 {
+
+    public static Vector3 With(this Vector3 original, float? x = null, float? y = null, float? z = null)
+        => new Vector3(
+            (float)(x == null ? original.x : x),
+            (float)(y == null ? original.y : y),
+            (float)(z == null ? original.z : z)
+            );
+
+    public static float Distance(this Vector3 from, Vector3 to)
+        => Vector3.Distance(from, to);
+
     public static bool IsGrounded(this Vector3 position, float offset)
         => Physics2D.Raycast(position, -Vector3.up, offset);
     public static bool IsGrounded(this Vector3 position, Collider2D collider, float offset)

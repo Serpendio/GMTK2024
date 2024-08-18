efc3790e8c4ed49749f1333eed77bf2fd553a431
SSeelos <sebastian.seelos@gmx.de>
2024-08-18 18:35:31 +0200

    show size gizmo and adapt sslime size

--- Assets/_Scripts/Enemies/EnemyBehaviour.cs
+++ Assets/_Scripts/Enemies/EnemyBehaviour.cs
@@ -140,7 +140,9 @@ public class EnemyBehaviour : MonoBehaviour
     {
         Gizmos.DrawWireSphere(this.body.position, Data.AwarenessRange);
         Gizmos.DrawWireSphere(this.body.position, Data.IdleRange);
-        
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireSphere(this.body.position, Data.Size);
+
         Gizmos.color= Color.red;
         Gizmos.DrawLine(this.body.position, body.position + Vector2.right * offsetX);
         Gizmos.DrawLine(this.body.position, body.position + Vector2.left * offsetX);

b74c93e086f192f0258ad44e3900ff10718a25ce
Serpendio <58090288+Serpendio@users.noreply.github.com>
2024-08-19 07:07:27 +0100

    Fixed a couple settings, modified lvl1

--- Assets/_Scripts/Enemies/EnemyDestroy.cs
+++ Assets/_Scripts/Enemies/EnemyDestroy.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class EnemyDestroy : MonoBehaviour
 {
+    private void Awake()
+    {
+        EnemyHander enemyHander = FindAnyObjectByType<EnemyHander>();
+        enemyHander.NubOFEnemys++;
+    }
+
     private void OnDestroy()
     {
         EnemyHander enemyHander = FindAnyObjectByType<EnemyHander>();
